292a292ed34122ff9924319b9c0779e4022992aa
Jannick Oursin <23587699+tram98@users.noreply.github.com>
2023-04-17 23:48:29 +0200

    added Jpeg-xr implementation base

---
+++ jpeg-xr/JpegXr/JpegXrDecoder.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace Org.OpenOrbit.Libraries.JpegXr
+{
+    public static class JpegXrDecoder
+    {
+        public static void Decode(Stream stream)
+        {
+            JpegXrFile.Decode(stream);
+        }
+    }
+}
